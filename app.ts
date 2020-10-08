b820746407ac8e786167ab7b901f3d23efb878ea
thelittlecitizen13 <thelittlecitizen13@gmail.com>
2020-10-08 22:49:38 +0300

    configuration file & cors handling added

--- app.ts
+++ app.ts
@@ -1,14 +1,21 @@
 import express = require('express');
+import cors = require('cors');
+import config = require('./configuration.json')
 const app: express.Application = express();
 
-const port = 9000
+const port = config.server.port;
+var coreOptions = {
+  origin: config.corsAllowed,
+  optionsSuccessStatus: 200
+}
 
+app.use(cors(coreOptions));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(require('./router'));
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`listening at http://localhost:${port}`)
 })
 
 
